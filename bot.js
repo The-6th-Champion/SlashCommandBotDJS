32d213e615718f728960f2cc4b2d12224a775a8c
sid-ms <67034744+sid-ms@users.noreply.github.com>
2021-03-03 19:32:06 -0800

    a couple commands and stuff

--- bot.js
+++ bot.js
@@ -1,24 +1,54 @@
 const botconfig = require('./botconfig.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const command = require('./command');
+const firstMessage = require('./first-message');
+
+client.on('ready', () => {
+    console.log('Im online ya know')
+    // just a test
+    firstMessage(client, '816869030681509958', 'hello there', ['⚙','👀'])
+
+    command(client, ["ping", "pong"], (message) => {
+        message.channel.send('Pong!')
+    })
+
+    command(client, 'serverlist', (message) => {
+        client.guilds.cache.forEach((guild) => {
+            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members`)
+        })
+    })
+
+    //Moderation
+    command(client, ['cc', 'clear channel'], message => {
+        if (message.member.hasPermission('ADMINISTRATOR')) {
+            message.channel.messages.fetch().then(results => {
+                message.channel.bulkDelete(results)
+            })
+        }
+    })
+
+    command(client, 'status', message => {
+        if (message.member.id == 654142589783769117) {
+            const content = message.content.replace('/status ', '')
+
+            client.user.setPresence({
+                activity: {
+                    name: content,
+                    type: 0
+                }
+            })
+            message.channel.send(`Done. Presence has been set to **Playing ${content}**`)
+        }
+        else {
+            message.channel.send("You are not **The 6th Champion**")
+        }
+        
+    })
+});
 
 
 
-client.on('ready', readyDiscord);
-
-function readyDiscord() {
-    console.log('Im online ya know');
-}
-
-client.on('message', gotMessage); 
-
-function gotMessage(msg) {
-    if (msg.content === 'champion bot') {
-        msg.reply('You called?');
-    } else if (msg.content === 'é' && msg.author != client.user) {
-        msg.channel.send('é');
-    }
-}
 // index.js
 const dotenv = require('dotenv');
 dotenv.config();
