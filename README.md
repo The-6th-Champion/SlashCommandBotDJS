9028d7999b7f397c55f01f834a7e3a2de66212a6
sid-ms <67034744+sid-ms@users.noreply.github.com>
2021-03-09 18:15:33 -0800

    only slash can remain

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ I made this bot as practice, nothing more, so no invite link this time.
 
 Maybe if it is good i may make a public copy 🙃
 
-This bot is also a way for me to add slash commadns to my server.
+This bot is also a way for me to add slash commands to my server.
--- bot.js
+++ bot.js
@@ -2,55 +2,198 @@ const botconfig = require('./botconfig.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const command = require('./command');
-const firstMessage = require('./first-message');
+const dotenv = require('dotenv').config();
+const guildID = '764927590070353940';
 
-client.on('ready', () => {
+
+const getApp = (guildID) => {
+    const app = client.api.applications(client.user.id)
+    if (guildID) {
+        app.guilds(guildID)
+    }
+    return app
+}
+client.on('ready', async () => {
     console.log('Im online ya know')
-    // just a test
-    firstMessage(client, '816869030681509958', 'hello there', ['⚙','👀'])
+    client.user.setStatus('invisible').then(console.log).catch(console.error)
+    const commands = await getApp(guildID).commands.get()
+    console.log(commands)
 
-    command(client, ["ping", "pong"], (message) => {
-        message.channel.send('Pong!')
+
+
+
+    await getApp(guildID).commands.post({
+        data: {
+            name: "ping",
+            description: "A simple ping command",
+        },
     })
-
-    command(client, 'serverlist', (message) => {
-        client.guilds.cache.forEach((guild) => {
-            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members`)
-        })
-    })
-
-    //Moderation
-    command(client, ['cc', 'clear channel'], message => {
-        if (message.member.hasPermission('ADMINISTRATOR')) {
-            message.channel.messages.fetch().then(results => {
-                message.channel.bulkDelete(results)
-            })
+    await getApp(guildID).commands.post({
+        data: {
+            name: "botinfo",
+            description: "Prefixes, info, and general usage of bots here",
         }
     })
+    await getApp(guildID).commands.post({
+        data: {
+            name: 'info',
+            description: 'sends an embed',
+            options: [
+                {
+                    name: "Name",
+                    description: 'Your username',
+                    required: true,
+                    type: 3
+                },
+                {
+                    name: "Age",
+                    description: 'Your age',
+                    required: false,
+                    type: 4
+                },
+            ]
+        }
+    })
+    /* To delete commands: 
+    await getApp(guildID).commands('command_id').delete() 
+    */
 
-    command(client, 'status', message => {
-        if (message.member.id == 654142589783769117) {
-            const content = message.content.replace('/status ', '')
+    
+    client.ws.on('INTERACTION_CREATE', async (interaction) => {
+        const { name, options } = interaction.data
+        const command = name.toLowerCase()
 
-            client.user.setPresence({
-                activity: {
-                    name: content,
-                    type: 0
+        const args = {}
+
+        if (options) {
+            for (const option of options) {
+                const { name, value } = option
+                args[name] = value
+            }
+        }
+
+        switch(command) {
+            case "ping":
+                reply(interaction, "pong");
+            case "info":
+                const embed = new Discord.MessageEmbed()
+                .setTitle("Info")
+
+                for (const arg in args) {
+                    const value = args[arg]
+                    embed.addField(arg, value)
                 }
-            })
-            message.channel.send(`Done. Presence has been set to **Playing ${content}**`)
-        }
-        else {
-            message.channel.send("You are not **The 6th Champion**")
-        }
-        
+                reply(interaction, embed)
+            case "botinfo":
+                const embed1 = new Discord.MessageEmbed()
+                .setTitle("Bot Info")
+                .setDescription("There are many bots in this server, and I mean **MANY**. If you have a bot you made you want to add, or you think one bot is a good idea to add, DM the owner.")
+                .addFields(
+                    {
+                        name: "<Insert Bot Here>",
+                        value: "This Bot's prefix is `>>`. It is a bot made by The 6th Champion. Invite it to your servers :)\n"
+                    },
+                    {
+                        name: "<Champion of the Botz>",
+                        value: "This Bot's prefix is `>`. It is a bot made by The 6th Champion for this server.\n"
+                    },
+                    {
+                        name: "Haptot",
+                        value: "JOKE\n"
+                    },
+                    {
+                        name: "Moyai",
+                        value: "This Bot's prefix is `]]`. 🗿🗿🗿\n"
+                    },
+                    {
+                        name: "QuoteAI",
+                        value: "This Bot's prefix is `*`. It gives you random inspirational quotes\n"
+                    },
+                    {
+                        name: "Plasma",
+                        value: "```diff\n-TO BE KICKED\n``` This Bot's prefix is `=`. It keeps track of the amount of people you invite, and gives you roles\n"
+                    },
+                    {
+                        name: "AmariBot",
+                        value: "This Bot's prefix is `a!`. It does levels\n"
+                    },
+                    {
+                        name: "Carl-Bot",
+                        value: "```diff\n-MIGHT BE KICKED\n``` This Bot's prefix is `c!`. No idea why its here.\n"
+                    },
+                    {
+                        name: "Dyno",
+                        value: "```diff\n-TO BE KICKED\n``` This Bot's prefix is `d!`. Does a bunch of stuff.\n"
+                    },
+                    {
+                        name: "Rythm",
+                        value: "This Bot's prefix is `r!`. moosic is good\n"
+                    },
+                    {
+                        name: "UnbelievaBoat",
+                        value: "This Bot's prefix is `u!`. Fun stuff...play around with it\n"
+                    },
+                    {
+                        name: "counting human",
+                        value: "This Bot's prefix is `c?`. It is a bot made for us to count. look in <#799679622056902706>\n"
+                    },
+                    {
+                        name: "DISBOARD",
+                        value: "This Bot's prefix is `!d `. Bump the server so we can spread this community\n"
+                    },
+                    {
+                        name: "DSL",
+                        value: "This Bot's prefix is `no idea actually`. This bot is for the **D**iscord **S**erver **L**ist. vote this server [here](https://top.gg/servers/764927590070353940 '<Insert Server Here>')\n"
+                    },
+                    {
+                        name: "Gerald",
+                        value: "This Bot's prefix is `/`. le funny. talk with it in <#797580142279917598>\n"
+                    },
+                    {
+                        name: "Kali",
+                        value: "This Bot's prefix is `!`. A bot made by Isukali. Does some cool stuff, like compiling code.\n"
+                    },
+                    {
+                        name: "Other",
+                        value: "Any bots that aren't mentioned most likely aren't to be used at the moment. Ask Da6thChamp for more info."
+                    },
+
+
+
+                )
+                .setColor("#9effff")
+                
+                reply(interaction, embed1)
+          }
     })
-});
+})
+
+
+const reply = async (interaction, response) => {
+    let data = {
+        content: response,
+    }
+    
+    if (typeof response === 'object') {
+        data = await createAPIMEssage(interaction, response)
+    }
+    client.api.interactions(interaction.id, interaction.token).callback.post({
+        data: {
+            type: 4,
+            data,
+        },
+    })
+}
 
 
 
-// index.js
-const dotenv = require('dotenv');
-dotenv.config();
-// ... client setup (keep reading)
+const createAPIMEssage = async (interaction, content) => {
+    const { data, files } = await Discord.APIMessage.create(
+        client.channels.resolve(interaction.channel_id),
+        content
+    )
+    .resolveData()
+    .resolveFiles()
+    return { ...data, files }
+}
 client.login(process.env.TOKEN);
